afb30870fe791997de473dc982cbbbab7f158dcb
iNgredie <sandslash@bk.ru>
2023-01-27 00:05:40 +0400

    first commit

---
+++ cmd/umbrella-test-task/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"idealTestTask/intenal/pkg/app"
+	"log"
+)
+
+func main() {
+	a, err := app.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = a.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
